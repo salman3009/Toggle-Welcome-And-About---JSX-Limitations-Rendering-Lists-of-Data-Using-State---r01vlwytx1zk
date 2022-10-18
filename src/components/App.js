3bbe0b79576912ccd931095cca2de4bd9008feb6
salman3009 <salman444g@gmail.com>
2022-10-18 22:17:13 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/App.css";
 
 const App = () => {
-
-  const [getFlag,setFlag] = useState(false);
-
-  const onclickHandler=()=>{
-    setFlag(!getFlag);
-  }
+  const [welcome, setWelcome] = React.useState(true);
 
   return (
     <div id="main">
-     {getFlag && <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div> } 
-      {!getFlag &&
-           <div id="about-div">
-           <h1>
-             Newton School is a neo-university providing highly immersive tech
-             learning to millions of students, to tap into new-age tech
-             opportunities.
-           </h1>
-         </div> 
-      }
- 
-      <button onClick={onclickHandler} id="toggle">Toggle</button>
+      {welcome ? (
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
+      ) : (
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
+      )}
+      <button id="toggle" onClick={() => setWelcome(!welcome)}>
+        Toggle
+      </button>
     </div>
   );
 };
